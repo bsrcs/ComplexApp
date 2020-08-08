0ad47b2ad880e593779f7fcc1d289c0bfb4aa731
bsrcs <bsr.ercelik@gmail.com>
2020-08-08 13:20:03 +0300

    Route through components

--- app/Main.js
+++ app/Main.js
@@ -1,19 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 // My Components
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 // ctrl+D ye basarak butun "class"lari "className"e cevir.
+// base url of our domain is "/"
 function Main(){
   return(
-    <>
-    <Header />
-    <HomeGuest />
-    <Footer />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 ReactDOM.render( <Main />, document.querySelector("#app"));
---
+++ app/components/Container.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+// set home component width to be wide
+// set "about-us" & "terms" page to be narrow 
+function Container(props) {
+  return (
+    <div className={"container py-md-5 " + (props.wide ? '' : 'container--narrow')}>
+      {props.children}
+    </div>
+  )
+}
+
+export default Container
